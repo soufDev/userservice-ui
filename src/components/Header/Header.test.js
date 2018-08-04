bce08551d8dfcf770ecca2c489aa5b16f7607a26
soufiane <ait.akkache.soufiane@gmail.com>
2018-08-04 03:12:05 +0200

    refactor some code for eslint

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
+/* eslint import/no-extraneous-dependencies:0 */
 import ShallowRender from 'react-test-renderer/shallow';
 import Header from './Header';
 
 const renderer = new ShallowRender();
-renderer.render(<Header items={[]} />)
+renderer.render(<Header items={[]} />);
 const result = renderer.getRenderOutput();
 
 describe('Header Component', () => {
   it('check if the Header return a Menu Component', () => {
     expect(result.type.name).toBe('Menu');
     expect(result.props.fixed).toBe('top');
-  })
+  });
 });
